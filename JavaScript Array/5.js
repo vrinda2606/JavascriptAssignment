03821b6b1d8a6826b133ee182f8ae7f62c722c57
Vrinda Sharma <86602126+vrinda2606@users.noreply.github.com>
2025-06-23 16:55:49 +0530

    Update and rename 5. to 5.js

--- JavaScript Array/5.js
+++ JavaScript Array/5.js
@@ -1,4 +1,4 @@
-const myColor = ["Red", "Green", "White", "Black"];
+jsconst myColor = ["Red", "Green", "White", "Black"];
 function func(myColor){
     console.log(myColor.join());
     console.log(myColor.join(' '));
